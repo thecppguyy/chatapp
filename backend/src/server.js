2be2f43b4385f0d2141e3c3f6dc24893345a17ae
thecppguyy <akashvishwakarma706853@gmail.com>
2026-02-13 02:31:09 +0530

    Modified server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,9 +24,9 @@ app.use("/api/message", messageRouter)
 import path from "path";
 const __dirname = path.resolve()
 if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../fontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
     app.get("/", (_, res) => {
-        res.sendFile(path.join(__dirname, "../fontend/dist/index.html"))
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
     })
 }
         
